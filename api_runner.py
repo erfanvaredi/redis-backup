54f05e1846466664266c15910fdc52bf42bcf5e4
erfanvaredi <erfanvaredi@outlook.com>
2022-12-10 23:34:12 +0400

    ✅ feat(api_runner): init fastapi app file

---
+++ api_runner.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+
+@app.get("/")
+def welcome():
+    return {'hello':'world'}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
