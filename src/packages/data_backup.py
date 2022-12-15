d2f1cbc0892c818639878b4eaa51ed7865500998
erfanvaredi <erfanvaredi@outlook.com>
2022-12-15 18:43:13 +0400

    ✨ feat: add async listener

--- src/packages/data_backup.py
+++ src/packages/data_backup.py
@@ -1,38 +1,38 @@
-from logging import getLogger
-
+import asyncio
+import redis.asyncio as redis
+from src.packages.utils import save_pickle_to_s3
+from datetime import datetime
 from boto3 import client
 from boto3_type_annotations.s3 import Client as S3Client
-from src.packages.utils import save_pickle_to_s3
-import settings
+import sys
+sys.path.insert(0,'..')
+
+from src import settings
+from logging import getLogger
 
 logger = getLogger(__name__)
 logger.addHandler(settings.handler)
 
 
-def run(s3_client: S3Client):
-    
-    if settings.DEBUG is False:
-        save_pickle_to_s3(
-            s3_client=s3_client,
-            bucket_name=settings.AWS_BUCKET_NAME,
-            key='something.pickle',
-            obj=,
-        )
+async def reader(channelPubSub: redis.client.PubSub, s3_client: S3Client):
+    while True:
+        message = await channelPubSub.get_message(ignore_subscribe_messages=True)
+        if message is not None:
+
+            message['datetime'] = datetime.utcnow().isoformat()
+            message['timestamp'] = datetime.utcnow().timestamp()
+            message['decoded_message'] = message['data'].decode()
+
+            if settings.DEBUG is False:
+                save_pickle_to_s3(
+                    s3_client=s3_client,
+                    bucket_name=settings.AWS_BUCKET_NAME,
+                    key=message['channel'].decode() + '_' + message['timestamp'] + '_' +'.pickle',
+                    obj=message,
+                )
+            
+            logger.info(f"message: {message} has been cloned to s3")
 
 
-    # logger.info("learning process just FINISHED", extra={"tags": {"module": "learner"}})
-
-
-if __name__ == "__main__":
-    """
-    Preparing connections to start training application.
-    """
-
-    s3_client: S3Client = client(
-        service_name="s3",
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_REGION_NAME,
-    )
-
-    run(s3_client)
+            if message["decoded_message"] == settings.REDIS_STOPWORD:
+                break
